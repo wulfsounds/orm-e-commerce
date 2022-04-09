2aa00f2f50f202b92f1fd5dc1402da7e5e41d4e1
Devin Wulf <devinlwulf@gmail.com>
2022-04-08 22:05:29 -0500

    created Catagory, Product, Tag, ProductTag

---
+++ models/ProductTag.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+const Product = require('./Product');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    }
+  },
+  {
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: id
+      }
+    }
+  },
+  {
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: tag,
+        key: id
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
