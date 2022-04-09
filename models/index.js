58ba00f4149a8a102c0ecbaad5d1ef3ae4ef93af
Devin Wulf <devinlwulf@gmail.com>
2022-04-08 22:32:30 -0500

    add models/index

--- models/index.js
+++ models/index.js
@@ -3,15 +3,35 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { triggerAsyncId } = require('async_hooks');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_name'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tagged_product'
+})
 
 module.exports = {
   Product,
